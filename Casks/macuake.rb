e0c9f6853ba27808261755f181df01c61a65eeeb
maksimnagaev <mnagaev@denti.ai>
2026-03-07 23:57:54 -0500

    Add macuake cask formula
    Initial cask for Macuake drop-down terminal.
    sha256 will be updated automatically by release workflow.

---
+++ Casks/macuake.rb
@@ -0,0 +1,18 @@
+cask "macuake" do
+  version "0.1.0"
+  sha256 :no_check
+
+  url "https://github.com/menemy/macuake/releases/download/v#{version}/Macuake.dmg"
+  name "Macuake"
+  desc "Drop-down Quake-style terminal for macOS powered by Ghostty"
+  homepage "https://macuake.com"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Macuake.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.macuake.terminal.plist",
+    "~/Library/Application Support/macuake",
+  ]
+end
